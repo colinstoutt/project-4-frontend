e15683ecab146fe9ca35c5ada313df004d19dd10
cps97 <colinstoutt@gmail.com>
2023-02-18 21:16:29 -0800

    display schedule data and style

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "../scss/Schedule.scss";
+import Game from "../components/Game";
 
-const Schedule = () => {
-  return <div>Schedule</div>;
+const Schedule = ({ team }) => {
+  return (
+    <div className="schedule">
+      <h1 className="schedule__heading">Schedule</h1>
+      <div className="schedule__line-divide"></div>
+      {team.games.map((item) => {
+        return (
+          <Game
+            key={item.id}
+            location={item.location}
+            date={item.date}
+            time={item.time}
+            homeTeam={item.home_team}
+            awayTeam={item.away_team}
+            team={team}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Schedule;
