59f0b06e7dd316f35d37741f625793daf2084010
cps97 <colinstoutt@gmail.com>
2023-02-18 18:59:30 -0800

    remove auth fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <Nav />
-      <Main />
+      <div className="main-container">
+        <Main />
+      </div>
     </div>
   );
 }
