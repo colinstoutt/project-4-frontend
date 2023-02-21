f2aa054d62e2978c0cf585bd9b9eef131ab39b94
cps97 <colinstoutt@gmail.com>
2023-02-20 18:49:27 -0800

    move fetch into App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
 
 function App(props) {
   const [team, setTeam] = useState(null);
+  const URL = "http://localhost:8000/manager/team/12/";
+
+  // INDEX
+  const getTeam = async () => {
+    const data = await fetch(URL).then((res) => res.json());
+    setTeam(data);
+  };
+
+  useEffect(() => {
+    getTeam();
+  }, []);
 
   return (
-    <div className="App" style={{ borderTop: `12px solid ${team.team_color}` }}>
+    <div
+      className="App"
+      style={
+        team
+          ? { borderTop: `12px solid ${team.team_color}` }
+          : { borderTop: `12px solid black` }
+      }
+    >
       <Nav team={team} />
       <div className="main-container">
-        <Main team={team} setTeam={setTeam} />
+        <Main team={team} setTeam={setTeam} getTeam={getTeam} />
       </div>
     </div>
   );
