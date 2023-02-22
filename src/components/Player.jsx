ea0f3d2787cf4adc9fa22b07a448727237791b07
cps97 <colinstoutt@gmail.com>
2023-02-21 18:55:59 -0800

    change table cell size, add edit icon

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import CancelIcon from "@mui/icons-material/Cancel";
+import { Link } from "react-router-dom";
+import EditIcon from "@mui/icons-material/Edit";
 
 const Player = (props) => {
   return (
@@ -7,30 +8,41 @@ const Player = (props) => {
       <tr className="roster__table-tr">
         <td className="roster__table-tr-td">{props.number}</td>
         <td className="roster__table-tr-td">
-          {props.first_name} {props.last_name}
+          {props.first_name} {props.last_name}{" "}
+          <Link to={`/roster/${props.id}/`}>
+            <EditIcon
+              className="roster_edit"
+              sx={{
+                fontSize: "0.8rem",
+                marginLeft: "0rem",
+                marginTop: "0.2rem",
+                color: "grey",
+                ":hover": { color: "black", cursor: "pointer" },
+              }}
+            />
+          </Link>
         </td>
         <td className="roster__table-tr-td">{props.position}</td>
         <td className="roster__table-tr-td">{props.age}</td>
         <td className="roster__table-tr-td">{props.contact}</td>
-        <td className="roster__table-tr-td roster__table-tr-td-status">
+        <td
+          className="roster__table-tr-td roster__table-tr-td-status"
+          style={{ width: "6rem" }}
+        >
           <span
             className="roster__status-pill"
             style={
               props.status === "Active"
                 ? {
-                    backgroundColor: "rgb(104, 219, 102, .3)",
                     border: "1px solid rgb(104, 219, 102)",
                   }
                 : {
-                    backgroundColor: "rgb(255, 195, 54, 0.3)",
                     border: "1px solid rgb(255, 195, 54)",
                   } && props.status === "Inactive"
                 ? {
-                    backgroundColor: "rgb(255, 195, 54, 0.3)",
                     border: "1px solid rgb(255, 195, 54)",
                   }
                 : {
-                    backgroundColor: "rgb(206, 66, 66, 0.3)",
                     border: "1px solid rgb(206, 66, 66)",
                   }
             }
@@ -38,14 +50,16 @@ const Player = (props) => {
             {props.status}
           </span>
         </td>
-        <td>
-          <button
-            className="roster__delete"
-            onClick={() => props.delete(props.id)}
-          >
-            Delete
-          </button>
-        </td>
+        <div className="edit-delete">
+          <td className="roster__table-tr-td roster__table-tr-td-status ">
+            <button
+              className="roster__delete"
+              onClick={() => props.delete(props.id)}
+            >
+              Delete
+            </button>
+          </td>
+        </div>
       </tr>
     </>
   );
