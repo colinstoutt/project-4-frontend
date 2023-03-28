73b896f6a5a78c0a3e2fd6aae1a680b972138568
cps97 <colinstoutt@gmail.com>
2023-03-28 13:52:44 -0700

    add conditional logic

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,54 +1,63 @@
 import "../scss/Nav.scss";
-import { Link, useNavigate } from "react-router-dom";
-import Roster from "../pages/Roster";
-import Schedule from "../pages/Schedule";
-import Recruitment from "../pages/Recruitment";
+import { Link } from "react-router-dom";
+import { getUserFromToken } from "../services/tokenService";
+const user = getUserFromToken();
 
 const Nav = ({ data, handleLogout }) => {
-  console.log(data);
-  return (
-    <div className="nav">
-      <div className="nav__main-links">
-        {data ? (
-          <Link to="/edit-team">
-            <img
-              className="nav__logo"
-              src={data.data[0].logo_url}
-              alt={data.data[0].mascot}
-            />
-          </Link>
-        ) : (
-          <div></div>
-        )}
-        <Link className="nav__link" to="/">
+  const loaded = () => {
+    const teams = data.data;
+    const userTeam = teams.filter((team) => team.user === user._id);
+
+    return (
+      <div className="nav">
+        <div className="nav__main-links">
           {data ? (
-            <span>
-              {data.data[0].city} {data.data[0].mascot}
-            </span>
+            <Link to="/edit-team">
+              <img
+                className="nav__logo"
+                src={userTeam[0].logo_url}
+                alt={`${userTeam[0].city} ${userTeam[0].mascot}`}
+              />
+            </Link>
           ) : (
-            <div>Team Summary</div>
+            <div></div>
           )}
-        </Link>
-        <Link className="nav__link" to="/roster">
-          <span>Roster</span>
-        </Link>
-        <Link className="nav__link" to="/schedule">
-          <span>Schedule</span>
-        </Link>
-        <Link className="nav__link" to="/recruitment">
-          <span>Recruitment</span>
-        </Link>
-        <Link to="/login">
-          <span
-            className="nav__logout nav__link"
-            onClick={() => handleLogout()}
-          >
-            Sign Out
-          </span>
-        </Link>
+          <Link className="nav__link" to="/">
+            {data ? (
+              <span>
+                {userTeam[0].city} {userTeam[0].mascot}
+              </span>
+            ) : (
+              <div>Team Summary</div>
+            )}
+          </Link>
+          <Link className="nav__link" to="/roster">
+            <span>Roster</span>
+          </Link>
+          <Link className="nav__link" to="/schedule">
+            <span>Schedule</span>
+          </Link>
+          <Link className="nav__link" to="/recruitment">
+            <span>Recruitment</span>
+          </Link>
+          <Link to="/login">
+            <span
+              className="nav__logout nav__link"
+              onClick={() => handleLogout()}
+            >
+              Sign Out
+            </span>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
+
+  const loading = () => {
+    return <div></div>;
+  };
+
+  return <div>{data ? loaded() : loading()}</div>;
 };
 
 export default Nav;
